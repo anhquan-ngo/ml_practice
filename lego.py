02a06e7ddeb82be7cf9326ee0540b9103ce8e2a5
anhquan-ngo <128469001+anhquan-ngo@users.noreply.github.com>
2023-03-22 08:34:41 +0700

    Update lego.py

--- lego.py
+++ lego.py
@@ -1,3 +1,36 @@
 ```
 Overview: Analyze a dataset on every single Lego block that has ever been built!
 ```
+import pandas as pd
+# Read colors data
+colors = pd.read_csv('datasets/colors.csv')
+
+# Print the first few rows
+print(colors.head)
+
+# How many distinct colors are available?
+num_colors = colors['name'].nunique()
+# Print num_colors
+print(num_colors)
+
+# colors_summary: Distribution of colors based on transparency
+colors_summary = colors.groupby(['is_trans']).count()
+
+%matplotlib inline
+# Read sets data as `sets`
+sets = pd.read_csv('datasets/sets.csv')
+
+# Create a summary of average number of parts by year: `parts_by_year`
+parts_by_year = sets[['year','num_parts']].groupby('year').mean()
+
+# Plot trends in average number of parts by year
+parts_by_year.plot()
+
+# themes_by_year: Number of themes shipped by year
+themes_by_year = sets[['year','theme_id']].groupby('year').nunique()
+print(themes_by_year.head)
+
+# Get the number of unique themes released in 1999
+num_themes = themes_by_year.loc[1999]
+# Print the number of unique themes released in 1999
+print(num_themes)
